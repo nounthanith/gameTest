07804fb6734cc5f1f2dae3611abbd66d382da897
nounthanith <nounthanith99@gmail.com>
2025-04-07 20:16:50 +0700

    update code

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,17 @@ public class Main {
         int boardHeight = rowCount * tileSize;
 
         JFrame frame = new JFrame("Pac Man");
-        frame.setVisible(true);
+//        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
 
     }
 }
